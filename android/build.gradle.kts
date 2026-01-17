31a5af5cf11ae30018c9479c93b474e97b3b1c5c
Anh Do <anhdo@example.com>
2026-01-17 23:07:30 +0700

    chore: upgrade AGP to 8.9.1 and Gradle to 8.11.1 for deploy v1.4.7

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -4,9 +4,9 @@ buildscript {
         mavenCentral()
     }
     dependencies {
-        classpath("com.google.gms:google-services:4.4.0")
-        classpath("com.android.tools.build:gradle:8.1.1")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.22")
+        classpath("com.google.gms:google-services:4.4.2")
+        classpath("com.android.tools.build:gradle:8.9.1")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.1.0")
     }
 }
 
